c601d82e8c554d21c6446285f47a89d3f37e1836
PERFEXION <chubuike17@gmail.com>
2026-05-18 19:20:32 +0100

    feat: update firebase configuration - update loading img width

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,7 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+import {
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+// 2. Fix the version mismatch by aligning Analytics with 10.8.1
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAi03Zknt670iQOaYpzw-s_mfXgu8pr83M",
@@ -11,9 +16,16 @@ const firebaseConfig = {
   messagingSenderId: "9259468189",
   appId: "1:9259468189:web:ba1c1280c391c859781b74",
   measurementId: "G-RPT3C1F3Z5",
+  databaseURL: "https://zavvy-app.firebaseio.com",
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager(),
+  }),
+  experimentalForceLongPolling: true,
+});
