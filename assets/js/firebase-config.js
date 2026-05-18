1e46cfaf39d6479dd2b7f9788a59af27659eb260
PERFEXION <chubuike17@gmail.com>
2026-05-18 16:26:28 +0100

    feat: update firebase config - add firebase hosting files

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAO8h-zpGL2dsv436HiTZuzL57WONjSfLg",
-  authDomain: "preparena-a5fa2.firebaseapp.com",
-  projectId: "preparena-a5fa2",
-  storageBucket: "preparena-a5fa2.firebasestorage.app",
-  messagingSenderId: "281707659217",
-  appId: "1:281707659217:web:a06449475aca7d912beaf0",
-  measurementId: "G-J8C71VG0VE",
+  apiKey: "AIzaSyAi03Zknt670iQOaYpzw-s_mfXgu8pr83M",
+  authDomain: "zavvy-app.firebaseapp.com",
+  projectId: "zavvy-app",
+  storageBucket: "zavvy-app.firebasestorage.app",
+  messagingSenderId: "9259468189",
+  appId: "1:9259468189:web:ba1c1280c391c859781b74",
+  measurementId: "G-RPT3C1F3Z5",
 };
 
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
